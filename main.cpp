85bd68e57eef58105c96ae5eb8347c1c79d36c1d
CodyAbad <caabad3@alaska.edu.com>
2020-04-06 00:35:44 -0800

    makes a snowman and saves it to a file

---
+++ main.cpp
@@ -0,0 +1,30 @@
+//main.cpp
+
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+using std::cout;
+using std::cos;
+using std::sin;
+#include "Shape.hpp"
+
+int main()
+{
+    std::ofstream fout("snowman.ps");
+
+    auto head = makeCircle(0.5);
+    auto body = makeCircle(1.0);
+    auto arm = makeRectangle(2.0, 0.2);
+    auto booty = makeCircle(2.0);
+    auto mid = makeHorizontalShape({ arm,body,arm });
+    auto snowMan = makeVerticalShape({ booty,mid,head});
+
+    fout << "%! \n288 288 translate\n";
+    snowMan->generatePostScript(fout);
+    fout << "\nshowpage \n\n\n";
+    fout.close();
+    cout << "Shape drawn successfully :)";
+    return 0;
+}
